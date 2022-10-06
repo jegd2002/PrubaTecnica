a3f623e9e985790f30594e27e3f8ce0778e4e659
Javier Diaz <jaedgodi20@gmail.com>
2022-10-06 18:26:18 -0500

    Commit Prueba Tecnica

---
+++ pruebaTecnica/pruebaTecnica/Models/productos.cs
@@ -0,0 +1,14 @@
+﻿namespace pruebaTecnica.Models
+{
+    public class productos
+    {
+        public int Id { get; set; } 
+        public string codigo { get; set; }    
+        public string descripcion { get; set; } 
+        public int[] listaDePrecios { get; set; }
+        public string imagen { get; set; }
+        public Boolean productoParaLaVenta { get; set; }
+        public int porcentajeIva { get; set; }
+
+    }
+}
